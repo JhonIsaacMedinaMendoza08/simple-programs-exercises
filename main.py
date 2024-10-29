119a6097646d7fb1da6429b5ad40e03835dc167a
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-29 13:36:50 -0500

    Solucion ejercicio de pitágoras

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-#Escriba un programa que pida al usuario un entero de tres dígitos, y entregue el número con los dígitos en orden inverso:
-#Ingrese numero: 345
-#543
-#Ingrese numero: 241
-#142
+#Escriba un programa que reciba como entrada las longitudes de los dos catetos a y b
+ #de un triángulo rectángulo, y que entregue como salida el largo de la hipotenusa c
+ #del triangulo, dado por el teorema de Pitágoras: c2=a2+b2
 
-number = input("Insert number, please: ")
-inverted_number = number[::-1]
-print(inverted_number)
-number2 = input("Insert number, please: ")
-inverted_number2 = number2[::-1]
-print(inverted_number2)
+#Ingrese cateto a: 7
+#Ingrese cateto b: 5
+#La hipotenusa es 8.6023252670426267
+
+
+import math
+
+cathetusA= int(input("Insert cathetus A , please: "))
+cathetusB= int(input("Insert cathetus B , please: "))
+hypotenuse = math.sqrt((cathetusA ** 2)+(cathetusB ** 2))
+
+print(f"""The hypotenuse is: {hypotenuse}
+""")
