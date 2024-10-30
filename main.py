cb82ae88529e63e2d7b81c2988ad434e296fb984
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-30 10:17:54 -0500

    Solucion ejercicio qué nota necesito

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-#Parte decimal
-#Escriba un programa que entregue la parte decimal de un número real ingresado por el usuario.
+#Un alumno desea saber que nota necesita en el tercer certamen para aprobar un ramo. El promedio del ramo se calcula con la siguiente formula.
 
-#Ingrese un numero: 4.5
-#0.5
-#Ingrese un numero: -1.19
-#0.19
+#Escriba un programa que pregunte al usuario las notas de los dos primeros certamen y la nota de laboratorio, y muestre la nota que necesita el alumno para aprobar el ramo con nota final 60.
 
-def decimal(number):
-    if number < 0:
-        return abs(number) - int(abs(number))
-    return number - int(number)
+#Ingrese nota certamen 1: 45
+#Ingrese nota certamen 2: 55
+#Ingrese nota laboratorio: 65
+#Necesita nota 72 en el certamen 3
 
-number2 = float(input("please, enter the number: "))
-decimal2 =  decimal(number2)
+def scoreNece(c1, c2, lab):
+    NF_deseada = 60
+    NC = (c1 + c2 + 0) / 3  
+    C3 = ((NF_deseada - lab * 0.3) / 0.7) * 3 - c1 - c2
+    return C3
 
-print(decimal2)
+score1 = float(input("Insert score contest 1: "))
+score2 = float(input("Insert score contest 2: "))
+scoreLab = float(input("Insert score contest LAB: "))
+
+score3final = scoreNece(score1, score2, scoreLab)
+
+print(f"You need {score3final:.2f} on the final exam 3")
+
 
