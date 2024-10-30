851ad4a1ff522efb2ae5baf40641bdd341bfd9d5
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-30 11:49:52 -0500

    Solucion ejercicio nota que necesito correcto

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-import math
+#Un alumno desea saber que nota necesita en el tercer certamen para aprobar un ramo. El promedio del ramo se calcula con la siguiente formula.
 
-Temp_inicial = int(input("Enter the initial temperature of the egg: "))
-MG = 67  # Mass in grams for a large egg
-MP = 47  # Mass in grams for a small egg
-P = 1.038  # density g/cm^3
-c = 3.7  # Heat capacity in J/(g K)
-K = 5.4 * math.pow(10, -3)  # Thermal conductivity in W/(cm·K)
-Tw = 100  # Boiling point of water in °C
-Ty = 70  # Maximum yolk temperature in °Cç
+#Escriba un programa que pregunte al usuario las notas de los dos primeros certamen y la nota de laboratorio, y muestre la nota que necesita el alumno para aprobar el ramo con nota final 60.
+
+#Ingrese nota certamen 1: 45
+#Ingrese nota certamen 2: 55
+#Ingrese nota laboratorio: 65
+#Necesita nota 72 en el certamen 3
 
 
-Numerator = math.pow(MG, (2/3)) * (c * math.pow(P, (1/3)))
-Denominator = (K * math.pow (math.pi, 2))  * math.pow((4*math.pi) / 3,(2/3))
-R1 = Numerator/Denominator
+score1 = float(input("Insert score contest 1: "))
+score2 = float(input("Insert score contest 2: "))
+scoreLab = float(input("Insert score contest LAB: "))
 
-r2 = math.log(0.76*((Temp_inicial - Tw)/(Ty - Tw)))
 
-TimeSeg = R1 * r2
-TimeMin = round(TimeSeg/60)
+NC = (59.5 - (0.3 * scoreLab )) / 0.7
+C3 = (((3 * NC ) - (score1 + score2)+ 1))
+    
 
-print (f"""The maximum time to prepare it in the cup is {TimeSeg} seg    """)
-print (f"""The maximum time to prepare it in the cup is {TimeMin} min    """)
+print(f"You need {round(C3)} on the final exam 3")
