818964ce71ca6e4508ec67e5b91bbe30028314d5
jhonIsaacMedinaMendoza08 <186082473+JhonIsaacMedinaMendoza08@users.noreply.github.com>
2024-10-30 11:23:21 -0500

    Solucion ejercicio huevos a la copa - Correcto

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
-M = 67  # Mass in grams for a large egg
-rho = 1.038  # density g/cm^3
+import math
+
+Temp_inicial = int(input("Enter the initial temperature of the egg: "))
+MG = 67  # Mass in grams for a large egg
+MP = 47  # Mass in grams for a small egg
+P = 1.038  # density g/cm^3
 c = 3.7  # Heat capacity in J/(g K)
-K = 5.4e-3  # Thermal conductivity in W/(cm·K)
+K = 5.4 * math.pow(10, -3)  # Thermal conductivity in W/(cm·K)
 Tw = 100  # Boiling point of water in °C
-Ty = 70  # Maximum yolk temperature in °C
+Ty = 70  # Maximum yolk temperature in °Cç
 
 
-def time_to_boil(To):
-    numerator = (M**(2/3)) * c * (rho**(1/3)) * K * (3.141592653589793**2) * ((4 * 3.141592653589793 / 3)**(2/3))
-    
-    # log(a) - log(b) = log(a/b)
-    term1 = 0.76 * To - Tw
-    term2 = Ty - Tw
-    log_term = term1 / term2
+Numerator = math.pow(MG, (2/3)) * (c * math.pow(P, (1/3)))
+Denominator = (K * math.pow (math.pi, 2))  * math.pow((4*math.pi) / 3,(2/3))
+R1 = Numerator/Denominator
 
-    # Calculamos el tiempo
-    t = numerator * log_term
-    return t
+r2 = math.log(0.76*((Temp_inicial - Tw)/(Ty - Tw)))
 
-original_temperature = float(input("Enter the original temperature of the egg (°C): "))
-time = time_to_boil(original_temperature)
+TimeSeg = R1 * r2
+TimeMin = round(TimeSeg/60)
 
-print(f"The time required to reach the maximum temperature is:{time:.2f} seg")
-
+print (f"""The maximum time to prepare it in the cup is {TimeSeg} seg    """)
+print (f"""The maximum time to prepare it in the cup is {TimeMin} min    """)
